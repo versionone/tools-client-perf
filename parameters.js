83e640987443b0f38198d1dd191633beae64189e
rajiv <rajiv.delwadia@versionone.com>
2010-09-20 15:48:15 -0400

    Slightly tighter interval

--- parameters.js
+++ parameters.js
@@ -1,6 +1,7 @@
 //defaults.minRounds = 3
 //defaults.maxRounds = 15
-//defaults.targetInterval = 0.05
+defaults.targetInterval = 0.075
+defaults.showTimes = true
 
 var ticket = 'HFZlcnNpb25PbmUuV2ViLkF1dGhlbnRpY2F0b3IUAAAABWFkbWluZ4B0x+omzQj/Pzf0dSjKKxACA489QU+OFtAvLCa6Wa58'
 var bigProject = 'Scope:130025'
