8532dc14522ec98519f86d7a22a2b106e3fa8034
rajiv <rajiv.delwadia@versionone.com>
2010-09-20 15:36:56 -0400

    Parameters for V1Web102 data

--- parameters.js
+++ parameters.js
@@ -2,9 +2,9 @@
 //defaults.maxRounds = 15
 //defaults.targetInterval = 0.05
 
-var ticket = 'HFZlcnNpb25PbmUuV2ViLkF1dGhlbnRpY2F0b3IUAAAABWFkbWluVNihoHEXzQj/Pzf0dSjKKxBYLo70VUVBzrt8Hec6LX5a'
+var ticket = 'HFZlcnNpb25PbmUuV2ViLkF1dGhlbnRpY2F0b3IUAAAABWFkbWluZ4B0x+omzQj/Pzf0dSjKKxACA489QU+OFtAvLCa6Wa58'
 var bigProject = 'Scope:130025'
 var projectWithSchedule = 'Scope:82559'
-var sprint = 'Timebox:188547'
+var sprint = 'Timebox:188545'
 var scopeChangeBegin = '6/30/2010'
 var scopeChangeEnd = '8/31/2010'
