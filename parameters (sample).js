1b5dc87cc6a1baee032a1b95578e3c2bc47bc397
rajiv <rajiv.delwadia@versionone.com>
2010-09-20 16:14:41 -0400

    sample parameters file

---
+++ parameters (sample).js
@@ -0,0 +1,11 @@
+//defaults.minRounds = 3
+//defaults.maxRounds = 15
+defaults.targetInterval = 0.075
+//defaults.showTimes = true
+
+var ticket = '???'
+var bigProject = 'Scope:130025'					// Product Development
+var projectWithSchedule = 'Scope:82559'		// Products
+var sprint = 'Timebox:188545'						// 10.3 - 2
+var scopeChangeBegin = '6/30/2010'
+var scopeChangeEnd = '8/31/2010'
