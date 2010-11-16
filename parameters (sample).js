3fbf531ea391d6c230af0f1583d1678a217f1222
rajiv <rajiv.delwadia@versionone.com>
2010-11-16 11:53:58 -0500

    action menu scenarios

--- parameters (sample).js
+++ parameters (sample).js
@@ -5,7 +5,3 @@ defaults.targetInterval = 0.075
 
 var ticket = '???'
 var bigProject = 'Scope:130025'					// Product Development
-var projectWithSchedule = 'Scope:82559'		// Products
-var sprint = 'Timebox:188545'						// 10.3 - 2
-var scopeChangeBegin = '6/30/2010'
-var scopeChangeEnd = '8/31/2010'
